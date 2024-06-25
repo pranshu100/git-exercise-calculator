5d9552c59e370eca220f10e75757762f986259a1
Pranshu <pvarshney@calance.com>
2024-06-26 01:58:21 +0530

    added streamlit app file

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+from utils import add_function, sub_function, mult_function, div_function 
+st.title("Simple Arithmetic Operations")
+
+
+num1 = st.number_input("Enter first number", value=0.0)
+num2 = st.number_input("Enter second number", value=0.0)
+
+# User can choose an operation
+operation = st.radio("Choose an operation", ("Addition", "Subtraction", "Multiplication", "Division"))
+
+# Perform the selected operation
+if st.button("Calculate"):
+    if operation == "Addition":
+        result = add_function(num1, num2)
+    elif operation == "Subtraction":
+        result = sub_function(num1, num2)
+    elif operation == "Multiplication":
+        result = mult_function(num1, num2)
+    elif operation == "Division":
+        result = div_function(num1, num2)
+    
+    st.write(f"The result of {operation} is: {result}")
