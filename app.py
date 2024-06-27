8fdcd8bf76e4cebe479083395ef11229c210b23b
palashj3 <palash.jain-cw@otsuka-us.com>
2024-06-27 12:48:34 +0530

    Formatting app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import add_function, sub_function, mult_function, div_function, log_function,multi_sum_div, power_func
+from utils import (
+    add_function,
+    sub_function,
+    mult_function,
+    div_function,
+    log_function,
+    multi_sum_div,
+    power_func,
+)
+
 st.title("Simple Arithmetic Operations")
 
 
@@ -9,7 +18,18 @@ num1 = st.number_input("Enter first number", value=0.0)
 num2 = st.number_input("Enter second number", value=0.0)
 
 # User can choose an operation
-operation = st.radio("Choose an operation", ("Addition", "Subtraction", "Multiplication", "Division","Log",'Multi_sum_div',"Power"))
+operation = st.radio(
+    "Choose an operation",
+    (
+        "Addition",
+        "Subtraction",
+        "Multiplication",
+        "Division",
+        "Log",
+        "Multi_sum_div",
+        "Power",
+    ),
+)
 
 # Perform the selected operation
 if st.button("Calculate"):
@@ -22,10 +42,10 @@ if st.button("Calculate"):
     elif operation == "Division":
         result = div_function(num1, num2)
     elif operation == "Log":
-        result = log_function(num1,num2)
+        result = log_function(num1, num2)
     elif operation == "Multi_sum_div":
-        result =multi_sum_div(num1,num2)
-    elif operation == 'Power':
-        result= power_func(num1,num2)
+        result = multi_sum_div(num1, num2)
+    elif operation == "Power":
+        result = power_func(num1, num2)
 
     st.write(f"The result of {operation} is: {result}")
